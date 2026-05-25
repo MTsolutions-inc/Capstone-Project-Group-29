47f3f88ac63e3ef53f9ba8eb5dcb0b9f418b9c93
MTsolutions-inc <mtsoftfix.solutions@gmail.com>
2026-05-26 00:54:04 +0100

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
-import 
+import Hero from "./components/Hero";
+import VideoSection from "./components/VideoSection";
+import PlanetGrid from "./components/PlanetGrid";
+import FactsTable from "./components/FactsTable";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
+
+
