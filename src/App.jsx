0600a3efec81db0bc7baa2de82cd53ef946553dd
Domika2 <chidiminigod.com>
2026-05-28 02:44:06 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Hero from './components/Hero';
+import TableSection from "./TableSection/TableSection";
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <>
+
+      <Hero />
+      <TableSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
