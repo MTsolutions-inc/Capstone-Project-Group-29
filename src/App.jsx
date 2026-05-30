2110db7bce1b6ab3a3bf54fa74517d67fd1620a4
Bejide Oluwatosin <oluwatossynbejide@gmail.com>
2026-05-30 06:07:52 +0100

    Remove unused imports and App component

--- src/App.jsx
+++ src/App.jsx
@@ -17,25 +17,4 @@ function App() {
   );
 }
 
-export default App;
-
-import './App.css';
-import Hero from './components/Hero/Hero';
-import TableSection from "./components/TableSection/TableSection";
-import PlanetCard from "./components/PlanetCard/PlanetCard";
-import Footer from './components/Footer/Footer';
-
-function App() {
-  return (
-    <>
-
-      <Hero />
-      <TableSection />
-      <PlanetCard />
-      <Footer />
-    </>
-  );
-}
-
-export default App;
 
