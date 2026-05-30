64702f74e02e24e93abdadb83b748d3be7f594ed
Bejide Oluwatosin <oluwatossynbejide@gmail.com>
2026-05-30 06:09:06 +0100

    Remove Hero component content and return statement

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -24,23 +24,3 @@ export default function Hero() {
     </section>
   );
 }
-  return (
-    <section className="hero">
-      <div className="hero-text">
-        <h1>Explore Our Solar System Through Data</h1>
-
-        <p>
-          Understand the planets not just by names, but by measureable facts.
-        </p>
-
-        <div className="hero-buttons">
-          <button>Explore the Data</button>
-          <button className="outline">Contact Us</button>
-        </div>
-      </div>
-
-      <div className="hero-image">
-        <img src="/earth.png" alt="Earth Image"/>
-      </div>
-    </section>
-  );
