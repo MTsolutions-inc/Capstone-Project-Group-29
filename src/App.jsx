f2bf46294659208634739956e1f4699fa5747a41
Domika2 <chidiminigod.com>
2026-06-02 10:54:57 +0100

    My First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Hero from './components/Hero/Hero';
+import VideoSection from './components/VideoSection/VideoSection';
+import PlanetCard from './components/PlanetCard/PlanetCard';
+import TableSection from "./components/TableSection/TableSection";
+import FormSection from './components/FormSection/FormSection';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <VideoSection />
+      <TableSection />
+      <PlanetCard />
+      <FormSection />
+      <Footer />
+    </> // 👈 Added the missing slash here to close the fragment!
+  );
+}
+
+export default App;
